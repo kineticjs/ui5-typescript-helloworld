c27c05486b00e6e0677034e1a2ccfb7728aa2b15
Diana Pazheva <diana.pazheva@sap.com>
2023-06-05 12:48:57 +0300

    preview state using SAPUI5 1.115

--- webapp/test/integration/pages/App.ts
+++ webapp/test/integration/pages/App.ts
@@ -1,11 +1,11 @@
 import Opa5 from "sap/ui/test/Opa5";
 import Press from "sap/ui/test/actions/Press";
-import OPA_Extension from "../OPA_Extension"; // TODO: will will no longer be needed once a standard Opa5 call is there
+//import OPA_Extension from "../OPA_Extension"; // TODO: will will no longer be needed once a standard Opa5 call is there
 
 const viewName = "ui5.typescript.helloworld.view.App";
 
 export class AppPageActions extends Opa5 {
-	and: AppPageActions // TODO: will no longer be needed in the future (probably with the 1.115 types)
+	//and: AppPageActions // TODO: will no longer be needed in the future (probably with the 1.115 types)
 
 	iPressTheSayHelloWithDialogButton() {
 		return this.waitFor({
@@ -13,7 +13,7 @@ export class AppPageActions extends Opa5 {
 			viewName,
 			actions: new Press(),
 			errorMessage: "Did not find the 'Say Hello With Dialog' button on the App view"
-		}) as AppPageActions & jQuery.Promise; // TODO: will no longer be needed in the future (probably with the 1.115 types)
+		}) //as AppPageActions & jQuery.Promise; // TODO: will no longer be needed in the future (probably with the 1.115 types)
 	}
 
 	iPressTheOkButtonInTheDialog() {
@@ -23,13 +23,13 @@ export class AppPageActions extends Opa5 {
 			viewName,
 			actions: new Press(),
 			errorMessage: "Did not find the 'OK' button in the Dialog"
-		}) as AppPageActions & jQuery.Promise; // TODO: will no longer be needed in the future (probably with the 1.115 types)
+		}) //as AppPageActions & jQuery.Promise; // TODO: will no longer be needed in the future (probably with the 1.115 types)
 	}
 }
 
 
 export class AppPageAssertions extends Opa5 {
-	and: AppPageAssertions
+	//and: AppPageAssertions
 
 	iShouldSeeTheHelloDialog() {
 		return this.waitFor({
@@ -39,7 +39,7 @@ export class AppPageAssertions extends Opa5 {
 				Opa5.assert.ok(true, "The dialog is open");
 			},
 			errorMessage: "Did not find the dialog control"
-		}) as AppPageAssertions & jQuery.Promise; // TODO: will no longer be needed in the future (probably with the 1.115 types)
+		}) //as AppPageAssertions & jQuery.Promise; // TODO: will no longer be needed in the future (probably with the 1.115 types)
 	}
 
 	iShouldNotSeeTheHelloDialog() {
@@ -51,9 +51,9 @@ export class AppPageAssertions extends Opa5 {
 			success: function() {
 				Opa5.assert.ok(true, "No dialog is open");
 			}
-		}) as AppPageAssertions & jQuery.Promise; // TODO: will no longer be needed in the future (probably with the 1.115 types)
+		}) //as AppPageAssertions & jQuery.Promise; // TODO: will no longer be needed in the future (probably with the 1.115 types)
 	}
 }
 
-// TODO: new API in OPA, this will be a regular Opa5 call
-OPA_Extension.createPageObjects_NEW_OVERLOAD("onTheAppPage", AppPageActions, AppPageAssertions);
+Opa5.createPageObjects({"onTheAppPage": {actions: AppPageActions, assertions: AppPageAssertions}});
+//OPA_Extension.createPageObjects_NEW_OVERLOAD("onTheAppPage", AppPageActions, AppPageAssertions);
